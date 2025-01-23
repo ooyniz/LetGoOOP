f3ab0a45ef2a6fbab315add6b418be21cf552e85
easyspubjava <eunjong.park@gmail.com>
2025-01-23 09:40:10 +0900

    builder source update

--- src/builder/test/Student.java
+++ src/builder/test/Student.java
@@ -8,6 +8,13 @@ public class Student {
     private String major;
     private String phoneNumber;
 
+    public void showInfo(){
+        System.out.println("학번 :" + id);
+        System.out.println("이름 :" + name);
+        System.out.println("전공   :" + major);
+        System.out.println("전화번호 :" + phoneNumber);
+
+    }
 
     public static void main(String[] args) {
         builder.Student student = new builder.Student.Builder(1, "John Doe")
