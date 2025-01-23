2dd9a1f4c824ea6a03f003e9524aafc39f8cc389
easyspubjava <eunjong.park@gmail.com>
2025-01-23 09:24:52 +0900

    update

--- src/factorymethod/after/CarMain.java
+++ src/factorymethod/after/CarMain.java
@@ -6,17 +6,17 @@ public class CarMain {
 
         // 차주의 이름을 생성자의 매개변수로 받습니다.
         CarFactory factory = CarFactory.getFactory();
-        Car sonata = factory.createCar(CarName.SONATA.name, "민주");
+        Car sonata = factory.createCar(CarName.SONATA.name, "은미");
         Car grandeur = factory.createCar(CarName.GRANDEUR.name, "찬규");
-        Car genesis = factory.createCar(CarName.GENESIS.name, "재원");
+        Car genesis = factory.createCar(CarName.GENESIS.name, "유영");
 
         System.out.println(sonata);
         System.out.println(grandeur);
         System.out.println(genesis);
         System.out.println("===================");
         // 주인차를 찾아줍니다.
-        System.out.println(factory.searchMyCar("민주"));
+        System.out.println(factory.searchMyCar("은미"));
         System.out.println(factory.searchMyCar("찬규"));
-        System.out.println(factory.searchMyCar("재원"));
+        System.out.println(factory.searchMyCar("유영"));
     }
 }
